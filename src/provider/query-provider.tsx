f43694fd503de34d158d17745fef6face5065d9e
ducothepaladin <lin303393@gmail.com>
2025-06-29 22:19:29 +0630

    initial commit

---
+++ src/provider/query-provider.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+
+const client = new QueryClient();
+
+
+
+function QueryProvider({children}: {children: ReactNode}) {
+  return (
+    <QueryClientProvider client={client}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider;
