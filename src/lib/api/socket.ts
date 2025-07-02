9ae789ad5eba88628c5cfd8f480bac795af81eef
Htet Aung Lwin <dev.htetaunglwin@gmail.com>
2025-07-02 21:54:39 +0630

    register page

--- src/lib/api/socket.ts
+++ src/lib/api/socket.ts
@@ -1,13 +1,10 @@
 import AppConfig from "@/lib/appConfig";
 
-
 const WEBSOCKET_URL = AppConfig.BASE_WEBSOCKET_URL || "localhost:5000";
 
-
 const createSocket = () => {
-
-    return new WebSocket(WEBSOCKET_URL);
-}
+  return new WebSocket(WEBSOCKET_URL);
+};
 
 const socket = createSocket();
 
