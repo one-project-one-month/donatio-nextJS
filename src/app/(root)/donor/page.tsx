ef68b27467143108d8ef782c6ef4e37f42e64f5c
egon <121918108+EgonLh@users.noreply.github.com>
2025-07-02 09:30:26 +0630

    fixed:reformat file structure

--- src/app/(root)/donor/page.tsx
+++ src/app/(root)/donor/page.tsx
@@ -1,7 +1,3 @@
-import TabSplitter from "@/components/common/tab-splitter";
-import ActivityLists from "@/components/ui/activities-list";
-import EventLists from "@/components/ui/events-list";
-import OrgnizationLists from "@/components/ui/orgs-list";
 import DonationBanner from "@/features/user/components/banner/donation-banner";
 import SearchSection from "@/features/user/components/search/search-section";
 
@@ -10,9 +6,6 @@ function UserProfilePage() {
   return (
     <div className="w-full">
       <SearchSection />
-      {/* Events and Org */}
-      <TabSplitter tab1Label={"Events"} tab2Label={"Orgnizations"} tab3Label={"Activities"} tab1Content={<EventLists/>} tab2Content={<OrgnizationLists/>} tab3Content={<ActivityLists/>}/>
-
       <DonationBanner />
     </div>
   )
