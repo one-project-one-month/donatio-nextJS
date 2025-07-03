9f731e159590ccbb627faf5893baa6f6e8f74891
ducothepaladin <lin303393@gmail.com>
2025-07-03 13:35:25 +0630

    feat: organization admin request form added

--- src/features/user/components/form/donate-form-3.tsx
+++ src/features/user/components/form/donate-form-3.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { DonateFormData } from "./donate-form-popup";
 import FormFileDropZone from "@/components/common/form-inputs/form-file-drop";
 import { useState } from "react";
-import { Image, Trash2 } from "lucide-react";
+import { FilePlus2, Image, Trash2 } from "lucide-react";
 
 const formOneSchema = z.object({
   organization: z
@@ -90,6 +90,7 @@ function DonateForm3({ formData }: DonateForm3Props) {
             labelClass="md:text-lg font-semibold mb-1"
             wrapperClass="mb-5 mb:mb-3"
             placeholder="Enter amount"
+            required
             className="h-12"
           />
           <FormInput
@@ -100,17 +101,24 @@ function DonateForm3({ formData }: DonateForm3Props) {
             labelClass="md:text-lg font-semibold mb-1"
             wrapperClass="mb-5 mb:mb-3"
             placeholder="Enter phone number"
+            required
             className="h-12"
           />
-            <FormFileDropZone
-              name="screenShot"
-              type="file"
-              form={form}
-              label="Upload Receipt Screenshot"
-              labelClass="mb-1 font-semibold text-base"
-              wrapperClass="mb-3"
-              defaultFile={formData.screenShot}
-            />
+          <FormFileDropZone
+            name="screenShot"
+            type="file"
+            form={form}
+            label={
+              <span className="flex items-center gap-2">
+                <FilePlus2 className="w-5 h-5 text-primary" />
+                Upload Receipt Screenshot
+              </span>
+            }
+            labelClass="mb-1 font-semibold text-base"
+            wrapperClass="mb-3"
+            defaultFile={formData.screenShot}
+            required
+          />
           <Button className="w-full rounded-full py-6 md:py-8 md:mt-5">
             Donate Now
           </Button>
