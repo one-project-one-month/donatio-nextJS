a8dd13ccdc1c3a7e710501a46628890c7e79f671
ducothepaladin <lin303393@gmail.com>
2025-08-10 20:24:14 +0630

    fix: build error

--- src/app/(root)/admin/events/page.tsx
+++ src/app/(root)/admin/events/page.tsx
@@ -1,94 +1,12 @@
-"use client";
+import EventsPage from "@/features/admin/components/event-page";
+import React, { Suspense } from "react";
 
-import OrganizationTable from "@/components/common/organization-table";
-import TableImageHolder from "@/components/common/table-image-holder";
-import { Button } from "@/components/ui/button";
-import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
-import EventDetailsDrawer from "@/features/admin/components/event-details-drawer";
-import Pagination from "@/features/admin/components/pagination";
-import { useEvents } from "@/features/admin/hooks/organization.queries";
-import { Events } from "@/features/admin/types";
-import { EyeIcon } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-
-const EventsPage = () => {
-  const searchParams = useSearchParams();
-  const page = Number(searchParams.get("page")) || 1;
-
-  const { data, isLoading } = useEvents(page);
-
-  const columns = [
-    {
-      key: "title",
-      label: "Title",
-      render: (row: Events) => row.title,
-      width: "150px",
-    },
-    {
-      key: "start_date",
-      label: "Start Date",
-      render: (row: Events) => new Date(row.start_date).toLocaleDateString(),
-      width: "150px",
-    },
-    {
-      key: "end_date",
-      label: "End Date",
-      render: (row: Events) => new Date(row.end_date).toLocaleDateString(),
-      width: "150px",
-    },
-    {
-      key: "goal_amount",
-      label: "Goal Amount",
-      render: (row: Events) => `${row.target_amount}`,
-      width: "150px",
-    },
-    {
-      key: "image",
-      label: "Image",
-      render: (row: Events) => <TableImageHolder data={row.attachments} />,
-      width: "150px",
-    },
-    {
-      key: "actions",
-      label: "Actions",
-      render: (row: Events) => (
-        <div className="flex justify-center gap-1">
-          <Drawer direction="right">
-            <DrawerTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                title="Details"
-                className="text-blue-600 hover:text-blue-800"
-              >
-                <EyeIcon size={15} />
-              </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-              <EventDetailsDrawer event={row} />
-            </DrawerContent>
-          </Drawer>
-        </div>
-      ),
-      width: "150px",
-    },
-  ];
-
+function Page() {
   return (
-    <div className="p-10">
-      <OrganizationTable<Events>
-        data={data?.results ?? []}
-        columns={columns}
-        isLoading={isLoading}
-      />
-
-      <Pagination
-        itemCount={data?.count ?? 0}
-        pageSize={7}
-        currentPage={page}
-      />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <EventsPage />
+    </Suspense>
   );
-};
+}
 
-export default EventsPage;
+export default Page;
--- src/app/(root)/donor/organizations/page.tsx
+++ src/app/(root)/donor/organizations/page.tsx
@@ -1,43 +1,11 @@
-"use client";
-
-import OrganizationListing from "@/features/user/components/organization/organization-listing";
-import OrganizationListingHeader from "@/features/user/components/organization/organization-listing-header";
-import OrganizationListingSkeleton from "@/features/user/components/organization/organization-listing-skeleton";
-import { useGetOrganizations } from "@/features/user/hooks/donor-organization-queries";
-import usePagination from "@/hooks/use-pagination";
-import { showToast } from "@/lib/toast";
-import { useState } from "react";
-import { useDebounce } from "use-debounce";
+import OrganizationListingSection from "@/features/user/components/organization/organization-listing-section";
+import React, { Suspense } from "react";
 
 function Page() {
-  const [searchValue, setSearchValue] = useState<string>("");
-  const [debouncedSearchValue] = useDebounce(searchValue, 500);
-  const { page, setPage } = usePagination();
-  const {
-    data: organizations,
-    isLoading,
-    isError,
-  } = useGetOrganizations(page, 12, debouncedSearchValue);
-
-  if (isError) {
-    showToast.error("Error getting organizations");
-    <div>Error getting organizations</div>;
-  }
-
   return (
-    <div className="mx-auto p-4">
-      <OrganizationListingHeader searchValue={searchValue}
-        onSearchChange={(e) => setSearchValue(e.target.value)} />
-      {isLoading ? (
-        <OrganizationListingSkeleton />
-      ) : (
-        <OrganizationListing
-          data={organizations ?? null}
-          page={page}
-          setPage={setPage}
-        />
-      )}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrganizationListingSection />
+    </Suspense>
   );
 }
 
